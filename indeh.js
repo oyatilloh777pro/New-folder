7e59dc4e82d185c3d433ac6431227d26af93927f
Oyatilloh Qobiljonov <oyatillohqobiljonov81@gmail.com>
2024-05-08 11:51:49 +0500

    done

---
+++ indeh.js
@@ -0,0 +1,11 @@
+var ism = prompt("Ismingizni kiriting:");
+var spaceId = prompt("Space ID ni kiriting:");
+var spacePassword = prompt("Space parolingizni kiriting:");
+
+var obyekt = {
+    name: ism,
+    id: spaceId,
+    password: spacePassword
+};
+
+console.log(obyekt);
